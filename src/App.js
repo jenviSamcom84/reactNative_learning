1f041cb802eb52e883c33e96984e87c9b2330a6a
jenviSemcom84 <jenvi@samcomtechnobrains.com>
2020-04-14 15:47:29 +0530

    react native with redux call

--- src/App.js
+++ src/App.js
@@ -1,67 +1,45 @@
 import React, {Component} from 'react';
-import {StyleSheet, Image} from 'react-native';
-//library imports
-import {Container, Content, Header, Body} from 'native-base';
-import {DrawerNavigator, DrawerItems} from 'react-navigation';
-import SettingsScreen from './SettingsScreen';
-import HomeScreen from './HomeScreen';
-import Login from './Login';
+import {StyleSheet, Text, View} from 'react-native';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider, connect} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+//import {StackNavigator, TabNavigator} from 'react-navigation';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
 
-import images from './image/image';
+import reducer from './reducer';
+import RepoList from './RepoList';
+import Profile from './Profile';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com',
+  responseType: 'json',
+});
+
+const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
+
 export default class App extends Component {
   render() {
-    return <MyApp />;
+    const Drawer = createDrawerNavigator();
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <NavigationContainer>
+            <Drawer.Navigator initialRouteName="RepoList">
+              <Drawer.Screen name="RepoList" component={RepoList} />
+              <Drawer.Screen name="Profile" component={Profile} />
+            </Drawer.Navigator>
+          </NavigationContainer>
+        </View>
+      </Provider>
+    );
   }
 }
-
-const CustomDrawerContentComponent = props => (
-  <Container>
-    <Header style={styles.drawerHeader}>
-      <Body>
-        <Image style={styles.drawerImage} source={images.icon} />
-      </Body>
-    </Header>
-    <Content>
-      <DrawerItems {...props} />
-    </Content>
-  </Container>
-);
-const MyApp = DrawerNavigator(
-  {
-    // For each screen that you can navigate to, create a new entry like this:
-    Home: {
-      screen: HomeScreen,
-    },
-    Login: {
-      screen: Login,
-    },
-    Settings: {
-      screen: SettingsScreen,
-    },
-  },
-  {
-    initialRouteName: 'Login',
-    drawerPosition: 'left',
-    contentComponent: CustomDrawerContentComponent,
-    drawerOpenRoute: 'DrawerOpen',
-    drawerCloseRoute: 'DrawerClose',
-    drawerToggleRoute: 'DrawerToggle',
-  },
-);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  drawerHeader: {
-    height: 200,
-    backgroundColor: 'white',
-  },
-  drawerImage: {
-    height: 150,
-    width: 150,
-    borderRadius: 75,
+    backgroundColor: '#fff',
   },
 });
